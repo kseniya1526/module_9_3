b42d3c044a7ad774a0ca366ab87826a815d61c37
kseniya1526 <kseniak505@gmail.com>
2024-10-05 19:25:57 +0700

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,11 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+
+first_result = (abs(len(x) - len(y)) for x, y in zip(first, second) if len(x) != len(y))
+
+second_result = (len(first[x]) == len(second[x]) for x in range(min(len(first), len(second))))
+
+
+print(list(first_result))
+print(list(second_result))
